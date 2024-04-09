2bded3ed487a6fa5d3a6db015f3863b56bb58239
mandydocaps <161479700+mandydocaps@users.noreply.github.com>
2024-04-09 10:00:26 -0300

    Add files via upload

---
+++ nome.py
@@ -0,0 +1,9 @@
+ef nome():
+    nome = input("Digite o seu nome")
+    if not nome:
+        nome = "você"
+        print(f"Um para {nome}, um para mim")
+    else:
+        print(f"Um para {nome}, um para mim")
+
+nome()
